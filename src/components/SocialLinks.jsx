ac3d6be25a76818f59ed0e15356152c2ff640710
Abir Kumar Nandi <116013922+Abir-0007@users.noreply.github.com>
2024-09-16 01:06:41 +0530

    Updated SocialLinks.jsx
    resume fix

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "/My_Resume.pdf",
+      href: "/My-Resume.pdf",
       style: "rounded-br-md",
       download: true,
     },
